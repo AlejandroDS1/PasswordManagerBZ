25a9321ff81d9b6d55330b4a5bde71a3c286f066
AlejandroDS1 <acantelo7@alumnes.ub.edu>
2024-06-20 15:14:26 +0200

    Client file, for tests

--- Client/client.py
+++ Client/client.py
@@ -1,24 +1,62 @@
-import socket
-from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives.asymmetric import padding, rsa
 from cryptography.hazmat.primitives import serialization, hashes
+from cryptography.hazmat.backends import default_backend
+from base64 import b64encode
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    
-    s.connect(('127.0.0.1', 65432))
+import requests
+req = requests.get("http://localhost:5000/public_key")
 
-    msj = s.recv(4096)
+public_key = '\n' +  dict(req.json())['public_key'] # Add \n to avoid error
 
-    print(f"Clave publica {msj}")
+DATA= {"email": "test@test.com", "username": "Alejandro Cantero", "password" : "Si", "public_key": "algo"}
 
-    MSJ = "Esto es lo que deberia poder desencriptar el"
+public_key = serialization.load_pem_public_key(
+            data=public_key.encode(),
+            backend=default_backend()
+        )
 
-    encr = serialization.load_pem_public_key(msj).encrypt(MSJ.encode(), 
-            padding.OAEP(
-                mgf=padding.MGF1(algorithm=hashes.SHA256()),
-                algorithm=hashes.SHA256(),
-                label=None
-        ))
+password = 'Testing password # - ()!\'"·'.replace(r'"', r'\"') # replace the " to \"
 
-    s.sendall(encr)
+DATA["password"] = password
+enc = public_key.encrypt(
+    str(DATA).encode("utf-16"),
+    padding.OAEP(
+        mgf=padding.MGF1(algorithm=hashes.SHA256()),
+        algorithm=hashes.SHA256(),
+        label=None
+    )
+)
 
-    print(f"MSJ encriptado: {encr}")
+encrypted_message_base64 = b64encode(enc).decode('utf-8')
+
+# DATA["password"] = encrypted_message_base64
+
+
+print(f"Vamos a enviar: {str(DATA)}")
+
+msj = requests.post("http://localhost:5000/register", 
+                    data=encrypted_message_base64,
+                    headers={"Content-type": "application/json", "Accept" : "application/json"})
+
+print(msj.text)
+
+"""
+# Generar clave privada del servidor
+private_key = rsa.generate_private_key(
+    public_exponent=65537,
+    key_size=4096
+)
+
+pem_private_key = private_key.private_bytes(
+    encoding=serialization.Encoding.PEM,
+    format=serialization.PrivateFormat.TraditionalOpenSSL,  # o PrivateFormat.PKCS8
+    encryption_algorithm=serialization.NoEncryption()  # O utiliza BestAvailableEncryption(b"password") para cifrar
+)
+
+# Serializar la clave pública a formato PEM
+pem_public_key = private_key.public_key().public_bytes(
+    encoding=serialization.Encoding.PEM,
+    format=serialization.PublicFormat.SubjectPublicKeyInfo
+)
+
+"""
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 PasswordManagerBZ
+
+# TODO
+
+- [ ] Hash user password
+- [ ] Check if users public key is correct
+- [ ] \(Optional) ? encrypt public key
+- [ ] Get DB_NAME from .env
+- [ ] JWT token?, maybe just public_key will do | CONSIDER
